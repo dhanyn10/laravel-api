0cabd59b7c0e4108b3a38cd79079cc358140551e
dhanyn10 <dhanyn.dhan@gmail.com>
2019-03-20 14:50:19 +0700

    database settings

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => 'mysql',
     'connections' => [
         'sqlite' => [
             'driver' => 'sqlite',
@@ -11,11 +11,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'laravel_api',
+            'username' => 'root',
+            'password' => '',
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
